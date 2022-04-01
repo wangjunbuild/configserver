6f29a829b6e9373e442407b7540e032f2ff4f5c5
caobowen <bowen.cao@sealsfun.com>
2022-04-01 16:01:26 +0800

    【add】上传配置服务器

---
+++ src/main/java/io/chainedin/biclub/ConfigserverApplication.java
@@ -0,0 +1,18 @@
+package io.chainedin.biclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.ConfigServerApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
